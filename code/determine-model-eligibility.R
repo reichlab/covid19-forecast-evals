2986b7d2a26b66dccf354f9ff35773308a736940
Nicholas Reich <nick@schoolph.umass.edu>
2020-10-02 15:38:58 -0400

    complete pass at model eligibility data-frame.

--- code/determine-model-eligibility.R
+++ code/determine-model-eligibility.R
@@ -1,8 +1,16 @@
 ## determine model eligibility
-library(zoltr)  ##devtools::install_github("reichlab/zoltr")
+library(zoltr)  ## devtools::install_github("reichlab/zoltr")
 library(tidyverse)
 library(lubridate)
-source("../../covid19-forecast-hub/code/processing-fxns/get_next_saturday.R")
+source("../covid19-forecast-hub/code/processing-fxns/get_next_saturday.R")
+source("code/unit_timezero_forecast_complete.R")
+
+## maximum number of weeks missing that we allow before disqualifying a model
+MAXIMUM_MISSING_WEEKS <- 3
+UNITS_FOR_ELIGIBILITY <- read_csv("../../covid19-forecast-hub/data-locations/locations.csv") %>%
+    mutate(for_scoring = abbreviation %in% c("US", datasets::state.abb)) %>%
+    filter(for_scoring) %>%
+    pull(location)
 
 
 ## connect to Zoltar
@@ -32,40 +40,90 @@ primary_models <- models(zoltar_connection, project_url) %>%
     filter(notes %in% c("primary", "secondary"))
 
 # 2. obtain timezeroes for remaining models, eliminate ones that don't have the right dates
-date_filtered_models <- tibble(target_end_date_1wk_ahead=Date(), last_forecast_in_week=Date(), model=character())
+date_filtered_models <- tibble(model=character(), timezero=Date(), target_end_date_1wk_ahead=Date())
 for(i in 1:nrow(primary_models)) {
-    message(paste("starting model", primary_models[i,"model_abbr"]))
-    this_model_forecasts <- forecasts(zoltar_connection, primary_models[i,"url"]) %>%
-        mutate(timezero_date=Sys.Date())
-    for(j in 1:nrow(this_model_forecasts)) {
-        this_model_forecasts[j,"timezero_date"] <- timezero_info(zoltar_connection, this_model_forecasts[j,"timezero_url"])$timezero_date
-    }
-    
+    message(paste("** starting model", primary_models[i,"model_abbr"]))
+    this_model_forecasts <- forecasts(zoltar_connection, primary_models[i,"url"]) 
+
     ## is there a forecast for every required week?
     required_forecast_weeks <- this_model_forecasts %>% 
         right_join(timezero_weeks, by=c("timezero_date" = "timezero")) %>% 
-        mutate(forecast_exists = !is.na(forecast_data_url))%>%
+        mutate(forecast_exists = !is.na(forecast_data_url)) %>%
         group_by(target_end_date_1wk_ahead) %>% 
         summarize(n_forecasts = sum(forecast_exists))
     
-    ## if there are no forecasts for more than 3 weeks, this model is not eligible
-    if(sum(required_forecast_weeks$n_forecasts==0) > 3) {
+    ## if there are no forecasts for more than MAXIMUM_MISSING_WEEKS weeks, this model is not eligible
+    if(sum(required_forecast_weeks$n_forecasts==0) > MAXIMUM_MISSING_WEEKS) {
         next()
     } else {
         ## choose most recent forecast for each week
         timezeroes_to_select <- this_model_forecasts %>% 
-            right_join(timezero_weeks, by=c("timezero_date" = "timezero")) %>%
+            inner_join(timezero_weeks, by=c("timezero_date" = "timezero")) %>%
             group_by(target_end_date_1wk_ahead) %>%
             arrange(timezero_date) %>%
-            summarize(last_forecast_in_week = last(timezero_date)) %>%
-            ungroup() %>%
+            summarize(timezero = last(timezero_date)) %>%
             mutate(model = primary_models[i,"model_abbr"])
     }
     ## return table with model and timezeroes that are eligible for inclusion based only on dates 
     date_filtered_models <- bind_rows(date_filtered_models, timezeroes_to_select) 
 }
 
-# - query potentially eligible forecasts to see if they are eligible
+#######################################################################
+## query potentially eligible forecasts to see if they are eligible
 
+the_targets <- c(paste(1:4, "wk ahead cum death"), paste(1:4, "wk ahead inc death"))
+
+## store vectors of models to consider
+date_eligible_models <- unique(date_filtered_models$model)
+
+
+model_completes <- tibble(model=character(), timezero=Date(), target_end_date_1wk_ahead=Date(),
+    num_units_eligible_cum=numeric(), num_units_eligible_inc=numeric())
+
+for(i in 1:length(date_eligible_models)){
+    
+    model_to_query <- date_eligible_models[i]
+    fcasts_to_query <- filter(date_filtered_models, model==model_to_query)
+    
+    fcasts <- do_zoltar_query(zoltar_connection, 
+        project_url =  project_url,
+        is_forecast_query = TRUE,
+        models = model_to_query, 
+        targets = the_targets,
+        units = UNITS_FOR_ELIGIBILITY,
+        types = c("quantile"),
+        timezeros = fcasts_to_query$timezero) %>%
+        select(-c(cat, prob, sample, family, param1, param2,param3))
+    
+    if(nrow(fcasts)==0) 
+        next()
+    ## disqualifying omissions for a timezero
+    ##  - not all 4 targets for inc or cum
+    ##  - less than 25 locations forecasted for inc or cum targets
+    ##  - not all the quantiles for every prediction
+    
+    ## for each unit-timezero pair, compute a binary "was this prediction complete"
+    preds_to_eval <- fcasts %>%
+        group_by(model, unit, timezero) %>%
+        summarize(complete_cum=FALSE, complete_inc=FALSE)
+    
+    for(j in 1:nrow(preds_to_eval)){
+        preds_to_eval$complete_cum <- unit_timezero_forecast_complete(filter(fcasts, timezero==preds_to_eval$timezero[j]), type="cum")
+        preds_to_eval$complete_inc <- unit_timezero_forecast_complete(filter(fcasts, timezero==preds_to_eval$timezero[j]), type="inc")
+    }
+    
+    this_model_completes <- preds_to_eval %>% 
+        group_by(model, timezero) %>%
+        summarize(num_units_eligible_cum = sum(complete_cum), num_units_eligible_inc = sum(complete_inc)) %>%
+        mutate(target_end_date_1wk_ahead = get_next_saturday(timezero) + (wday(timezero)>2)*7)
+
+    
+    model_completes <- bind_rows(model_completes, this_model_completes)
+}
+
+model_completes <- complete(model_completes, model, target_end_date_1wk_ahead,
+    fill = list(timezero=NA, num_units_eligible_cum=0, num_units_eligible_inc=0)) 
+
+write_csv(model_completes, path="paper-inputs/model-eligibility.csv")
 
 ## output data.frame with list of models and eligibility
---
+++ code/unit_timezero_forecast_complete.R
@@ -0,0 +1,25 @@
+
+
+unit_timezero_forecast_complete <- function(dat, 
+    type = c("inc", "cum"), 
+    horizons = 1:4,
+    quantiles = c(0.01, 0.025, seq(0.05, 0.95, by=0.05), 0.975, 0.99)) {
+    
+    require(tidyverse)
+    
+    type <- match.arg(type)
+    
+    tmp_target <- paste(horizons, "wk ahead", type, "death")
+    
+    all_combos <- crossing(target = tmp_target, unit=unique(dat$unit), quantile=round(quantiles, digits=3))
+    missing_combos <- anti_join(all_combos, dat)
+
+    if(nrow(missing_combos)==0){
+        message("prediction complete")
+        return(TRUE)
+    } else {
+        message("prediction is incomplete")
+        print(missing_combos, n=10)
+        return(FALSE)
+    }
+}
