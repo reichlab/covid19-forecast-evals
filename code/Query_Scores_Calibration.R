0beb1c7df6c09d132c59ca2aa2a8bb6cf9072fd7
eycramer <ecramer@umass.edu>
2020-10-22 12:23:47 -0500

    add calibration csv files. Update query script

--- code/Query_Scores_Calibration.R
+++ code/Query_Scores_Calibration.R
@@ -7,34 +7,43 @@
 library(lubridate)
 library(zoltr)
 library(tidyverse)
+library(covidHubUtils) ##devtools::install_github("reichlab/covidHubUtils")
 
-# read in score files 
-cum_scores <- read_csv("../paper-inputs/20201013-cum-scores.csv") %>%
-  filter(location_name %in% datasets:: state.name)
-inc_scores <- read_csv("../paper-inputs/20201013-inc-scores.csv") %>%
-  filter(location_name %in% datasets:: state.name)
+data(hub_locations)
 
-#Query scores from Zoltar 
-zoltar_connection <- new_connection()
-zoltar_authenticate(zoltar_connection, Sys.getenv("Z_USERNAME"), Sys.getenv("Z_PASSWORD"))
 
-the_models_cum <- unique(cum_scores$model)
-the_models_inc <- unique(inc_scores$model)
+model_eligibility <- read_csv("paper-inputs/model-eligibility.csv")
 
-the_projects <- projects(zoltar_connection)
-project_url <- the_projects[the_projects$name == "COVID-19 Forecasts", "url"]
+## locations/dates with reporting anomalies
+dates_with_issues <- read_csv("paper-inputs/anomaly-reporting-dates.csv", col_types = "nDccDnnnc") %>%
+  filter(to_remove==1)
 
-the_locations <- c("01", "02", "04", "05", "06", "08", "09", "10", "12", 
+
+## models to pull forecasts for
+the_models_cum <- model_eligibility %>% filter(target_group=="cum") %>% pull(model) %>% unique()
+the_models_inc <- model_eligibility %>% filter(target_group=="inc") %>% pull(model) %>% unique()
+
+#Locations to pull forecasts for
+the_locations <- c("US", "01", "02", "04", "05", "06", "08", "09", "10", "12", 
                    "13", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", 
                    "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", 
                    "36", "37", "38", "39", "40", "41", "42", "44", "45", "46", "47", 
                    "48", "49", "50", "51", "53", "54", "55", "56")
 
+## the targets to pull forecasts for
 the_targets_cum <- paste(1:20, "wk ahead cum death")
 the_targets_inc <- paste(1:20, "wk ahead inc death")
 
-the_timezeros_cum <- seq(from = min(cum_scores$timezero), to = max(cum_scores$timezero), by="days")
-the_timezeros_inc <- seq(from = min(inc_scores$timezero), to = max(inc_scores$timezero), by="days")
+## Dates to extract from Zoltar
+the_timezeros_cum <- model_eligibility %>% filter(target_group=="cum") %>% pull(timezero) %>% unique()
+the_timezeros_inc <- model_eligibility %>% filter(target_group=="inc") %>% pull(timezero) %>% unique()
+
+#Query scores from Zoltar 
+zoltar_connection <- new_connection()
+zoltar_authenticate(zoltar_connection, Sys.getenv("Z_USERNAME"), Sys.getenv("Z_PASSWORD"))
+
+the_projects <- projects(zoltar_connection)
+project_url <- the_projects[the_projects$name == "COVID-19 Forecasts", "url"]
 
 cum_calibration1 <- do_zoltar_query(zoltar_connection,
                                     project_url =  "https://zoltardata.com/api/project/44/",
@@ -83,7 +92,17 @@ inc_calibration1 <- do_zoltar_query(zoltar_connection,
 inc_calibration2 <- do_zoltar_query(zoltar_connection,
                                     project_url =  "https://zoltardata.com/api/project/44/",
                                     is_forecast_query = TRUE,
-                                    models = the_models_inc[7:12],
+                                    models = the_models_inc[7:9],
+                                    units = the_locations,
+                                    targets = the_targets_inc,
+                                    timezeros = the_timezeros_inc,
+                                    types = c("quantile")) %>%
+  filter(quantile == .025 | quantile == .25 | quantile == .75 | quantile == .975)
+
+inc_calibration3 <- do_zoltar_query(zoltar_connection,
+                                    project_url =  "https://zoltardata.com/api/project/44/",
+                                    is_forecast_query = TRUE,
+                                    models = the_models_inc[10:12],
                                     units = the_locations,
                                     targets = the_targets_inc,
                                     timezeros = the_timezeros_inc,
@@ -94,8 +113,7 @@ inc_calibration2 <- do_zoltar_query(zoltar_connection,
 inc_calibration <- rbind(inc_calibration1, inc_calibration2, inc_calibration3) 
 
 #Write csv files with zoltar data 
-write.csv(cum_calibration, path = paste0("paper-inputs/", format(Sys.Date(), "%Y%m%d"), "-cum-calibration.csv"))
-write.csv(inc_calibration, path = paste0("paper-inputs/", format(Sys.Date(), "%Y%m%d"), "-inc-calibration.csv"))
-
+write_csv(cum_calibration, path = paste0("paper-inputs/", format(Sys.Date(), "%Y%m%d"), "-cum-calibration.csv"))
+write_csv(inc_calibration, path = paste0("paper-inputs/", format(Sys.Date(), "%Y%m%d"), "-inc-calibration.csv"))
 
 
