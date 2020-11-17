27b10ab5e2dccce78260f0dc0722edc11abf2890
Nicholas Reich <nick@schoolph.umass.edu>
2020-11-17 13:03:04 -0500

    removing unused data files, eliminating duplicative code that tries to keep only 1 forecast per week in process-zoltar-scores.R after this is already done in determine-model-eligibility.R

--- code/process-zoltar-scores.R
+++ code/process-zoltar-scores.R
@@ -26,34 +26,14 @@ inc_scores_calc <- inc_scores %>%
 
 ## keep only forecasts for eligible model-timezeros 
 cum_scores_eligible <- cum_scores_calc %>%
-  right_join(filter(model_eligibility_cum, target_group=="cum"))
-
+  right_join(filter(model_eligibility_cum, target_group=="cum")) %>%
+  mutate(target_end_date_1wk_ahead = as.Date(covidHubUtils::calc_target_week_end_date(timezero, horizon=1)))
+  
 inc_scores_eligible <- inc_scores_calc %>%
-  right_join(filter(model_eligibility_inc, target_group=="inc"))
-
-
-## add target_wk_end_date for all scores and remove second forecasts per week
-timezero_end_dates <- tibble(
-  timezero=unique(c(cum_scores_calc$timezero, inc_scores_calc$timezero)), 
-  target_end_date_1wk_ahead = as.Date(covidHubUtils::calc_target_week_end_date(timezero, horizon=1)))
-
-cum_scores_calc <- left_join(cum_scores_calc, timezero_end_dates) %>%
-  ## only keep 1 forecast per week
-  group_by(model, target_end_date_1wk_ahead) %>%
-  mutate(last_timezero = max(timezero)) %>%
-  filter(timezero == last_timezero) 
-
-inc_scores_calc <- left_join(inc_scores_calc, timezero_end_dates) %>%
-  ## only keep 1 forecast per week
-  group_by(model, target_end_date_1wk_ahead) %>%
-  mutate(last_timezero = max(timezero)) %>%
-  filter(timezero == last_timezero)
-
+  right_join(filter(model_eligibility_inc, target_group=="inc")) %>%
+  mutate(target_end_date_1wk_ahead = as.Date(covidHubUtils::calc_target_week_end_date(timezero, horizon=1)))
 
 ## write to disk
 write_csv(cum_scores_eligible, file = paste0("paper-inputs/cum-scores.csv"))
 write_csv(inc_scores_eligible, file = paste0("paper-inputs/inc-scores.csv"))
-
-write_csv(cum_scores_calc, file = paste0("paper-inputs/cum-scores_all.csv"))
-write_csv(inc_scores_calc, file = paste0("paper-inputs/inc-scores_all.csv"))
 
