fd7650c13513f7f13f875f6c179bebaf422a96df
eycramer <ecramer@umass.edu>
2020-11-18 15:37:40 -0600

    update to include territories and not US

--- code/get-calibration-scores-inc.R
+++ code/get-calibration-scores-inc.R
@@ -24,12 +24,12 @@ the_models_inc1 <- the_models_inc[1:(round(n_inc_models/2))]
 the_models_inc2 <- the_models_inc[(round(n_inc_models/2)+1):n_inc_models]
 
 #Locations to pull forecasts for
-the_locations <- c("US", "01", "02", "04", "05", "06", "08", "09", "10", "12", 
-  "13", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", 
-  "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", 
-  "36", "37", "38", "39", "40", "41", "42", "44", "45", "46", "47", 
-  "48", "49", "50", "51", "53", "54", "55", "56")
-
+the_locations <- hub_locations %>% filter(geo_type == "state") %>% 
+  filter(location_name != "United States" & location_name != "American Samoa") %>% 
+  pull(fips)
+  
+  
+  
 ## the targets to pull forecasts for
 the_targets_inc <- paste(1:20, "wk ahead inc death")
 
@@ -70,4 +70,4 @@ inc_calibration <- bind_rows(inc_calibration1, inc_calibration2)
 
 
 #Write csv files with zoltar data 
-write_csv(inc_calibration, file = "paper-inputs/inc-calibration.csv")
+write_csv(inc_calibration, path = "paper-inputs/inc-calibration.csv")
