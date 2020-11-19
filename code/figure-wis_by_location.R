a1dc4e5744bc85d7a1566d473f9819ce2e523d8e
eycramer <ecramer@umass.edu>
2020-11-19 15:16:03 -0600

    update scale

--- code/figure-wis_by_location.R
+++ code/figure-wis_by_location.R
@@ -1,6 +1,6 @@
 library(lubridate)
 library(tidyverse)
-
+library(covidHubUtils)
 theme_set(theme_bw())
 data("hub_locations")
 
@@ -16,7 +16,7 @@ average_by_loc <- inc_scores %>%
   group_by(location_name) %>%     
   mutate_at(vars(matches("avg_wis")), funs(relative_wis = (. / .[model=="COVIDhub-baseline"]))) %>% 
   ungroup() %>% 
-  mutate(log_relative_wis = ifelse(relative_wis == 0, 0, log(relative_wis))) 
+  mutate(log_relative_wis = ifelse(relative_wis == 0, 0, log2(relative_wis))) 
 
 average_by_loc$model<- reorder(average_by_loc$model, -average_by_loc$sum_truth) #sort models by WIS for plot
 average_by_loc$location_name <- reorder(average_by_loc$location_name, average_by_loc$avg_wis)
@@ -24,7 +24,7 @@ average_by_loc$location_name <- reorder(average_by_loc$location_name, average_by
 fig_wis_loc <- ggplot(average_by_loc, aes(x=model, y=location_name,fill= log_relative_wis)) +
   geom_tile() +
   geom_text(aes(label=round(avg_wis)), size = 3) +
-  scale_fill_gradient2(low = "navy", high = "red", midpoint = 0, na.value = "grey50", name = "log(model wis / baseline wis)")+ 
+  scale_fill_gradient2(low = "navy", high = "red", midpoint = 0, na.value = "grey50", name = "Relative WIS", breaks = c(-3,-2,-1,0,1,2,3), labels =c(0.125,0.25, 0.5, 1, 2, 4, 8))+ 
   xlab("Scored Models") + ylab("Location") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 10),
       axis.title.x = element_text(size = 9),
