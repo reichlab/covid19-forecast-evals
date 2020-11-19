9b33d11b068ec8b3d62132d940ffb0cab4bc56a8
eycramer <ecramer@umass.edu>
2020-11-18 20:46:44 -0600

    Heat map and code for wis by location

---
+++ code/figure-wis_by_location.R
@@ -0,0 +1,46 @@
+library(lubridate)
+library(tidyverse)
+
+theme_set(theme_bw())
+data("hub_locations")
+
+inc_scores <- read_csv("paper-inputs/inc-scores.csv") %>%
+  filter(location_name %in% (hub_locations %>% filter(geo_type == "state") %>% pull(location_name))) %>%
+  filter(location_name != "United States" & location_name != "American Samoa") %>%
+  mutate(wis = (.01*interval_2+.025*interval_5+.05*interval_10+.1*interval_20+.15*interval_30+.2*interval_40+.25*interval_50+.3*interval_60+.35*interval_70+.40*interval_80+.45*interval_90+.5*interval_100)/12)  %>% 
+
+
+average_by_loc <- inc_scores %>%
+  group_by(model, location_name) %>%  #aggregate by week of submission
+  summarise(avg_wis = round(mean(wis, na.rm = T),1)) %>% 
+  group_by(location_name) %>%     
+  mutate_at(vars(matches("avg_wis")), funs(relative_wis = (. / .[model=="COVIDhub-baseline"]))) %>% 
+  ungroup()
+
+average_by_loc$model<- reorder(average_by_loc$model, average_by_loc$avg_wis) #sort models by WIS for plot
+average_by_loc$location_name <- reorder(average_by_loc$location_name, average_by_loc$avg_wis)
+
+
+fig_wis_loc <- ggplot(average_by_loc, aes(x=model, y=location_name,fill=scales::oob_squish(round(relative_wis,2), range = c(-.5, 7)))) +
+  geom_tile() +
+  geom_text(aes(label=round(avg_wis)), size = 3) +
+  scale_fill_gradient2(low = "navy", high = "red", midpoint = 1, na.value = "grey50", name = "model wis / baseline wis")+ 
+  xlab("Scored Models") + ylab("Location") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 10),
+      axis.title.x = element_text(size = 9),
+      axis.text.y = element_text(size = 9),
+      title = element_text(size = 9)) 
+
+
+pdf(file = "figures/fig-wis-location.pdf",width=8, height=8)
+print(fig_wis_loc)
+dev.off()
+
+jpeg(file = "figures/fig-wis-location.jpg", width=8, height=6, units="in", res=300)
+print(fig_wis_loc)
+dev.off()
+
+
+
+  
+  
