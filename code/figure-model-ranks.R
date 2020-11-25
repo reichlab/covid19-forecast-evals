151d497f34b4d545a1946e62d50ba34dcfbd6394
Nicholas Reich <nick@schoolph.umass.edu>
2020-11-25 15:22:41 -0500

    adding model ranks figure and data revisions.

---
+++ code/figure-model-ranks.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(covidHubUtils)
+
+theme_set(theme_bw())
+data("hub_locations")
+
+inc_scores <- read_csv("paper-inputs/inc-scores.csv") %>%
+  filter(location_name != "American Samoa", target %in% paste(1:4, "wk ahead inc death")) %>%
+  mutate(id = paste(target_end_date_1wk_ahead, target, location_name)) %>%
+  group_by(target_end_date_1wk_ahead, target, location_name) %>%
+  arrange(wis) %>%
+  mutate(model_rank = row_number()) %>%
+  ungroup() %>%
+  mutate(model = reorder(model, -model_rank, FUN=function(x) mean(x, na.rm=TRUE)))
+
+
+# ggplot(inc_scores, aes(y=id, x=model, fill=model_rank)) +
+#   geom_tile() +
+# #  facet_grid(location_name + target_end_date_1wk_ahead + target ~.) +
+#   scale_fill_gradient(na.value="red") +
+#   scale_y_discrete(labels=element_blank()) +
+#   theme(
+#     axis.text.x = element_text(angle = 45, hjust = 1, size = 10),
+#     axis.title.y=element_blank(),
+#     axis.text.y=element_blank(),
+#     axis.ticks.y=element_blank())
+
+ggplot(inc_scores, aes(x=model, fill=factor(model_rank))) +
+  geom_bar()
+
