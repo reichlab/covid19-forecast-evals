da22972bf2dd4bc1a4cafc883eb1789354dab179
Nicholas Reich <nick@schoolph.umass.edu>
2020-12-07 17:12:50 -0500

    update code in global analysis dates to match the v1 of the paper, before updating the dates to include Sept/October.

--- code/load-global-analysis-dates.R
+++ code/load-global-analysis-dates.R
@@ -2,7 +2,7 @@ require(covidHubUtils)
 
 ## important dates for score calculations
 first_timezero <- as.Date("2020-05-12") ## earliest forecast_date in an eligible file 
-last_timezero <- as.Date("2020-08-31") ## latest forecast_date in an eligible file 
+last_timezero <- as.Date("2020-08-24") ## latest forecast_date in an eligible file  as.Date("2020-10-26")
 
 ## range of target_week_end_dates
 first_1wk_target_end_date <- as.Date(calc_target_week_end_date(first_timezero, 1))
@@ -14,14 +14,15 @@ last_4wk_target_end_date <- as.Date(calc_target_week_end_date(last_timezero, 4))
 the_timezeros_inc <- seq(from = first_timezero, to = last_timezero, by="days")
 
 ## maximum number of weeks missing that we allow before disqualifying a model
-MAXIMUM_MISSING_WEEKS <- 3
+MAXIMUM_MISSING_WEEKS <- 3 # 6
 UNITS_FOR_ELIGIBILITY <- covidHubUtils::hub_locations %>%
   mutate(for_scoring = abbreviation %in% c("US", datasets::state.abb)) %>%
   filter(for_scoring) %>%
   pull(fips)
 
 ## minimum number of weeks for eligibility
-NUM_WEEKS_INC <- 11
+num_weeks_forecasted <- length(seq.Date(first_1wk_target_end_date, last_1wk_target_end_date, by="7 days"))
+NUM_WEEKS_INC <- num_weeks_forecasted - MAXIMUM_MISSING_WEEKS
 
 ## minumum number of models in a week to be considered eligible in a given week
 NUM_UNITS <- 25
