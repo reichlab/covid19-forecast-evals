9247a5ff90371f6c6705af4f3d38d9b31466c99c
Nicholas Reich <nick@schoolph.umass.edu>
2020-12-09 17:11:27 -0500

    adding in some paper calculations

--- code/figure-model-ranks.R
+++ code/figure-model-ranks.R
@@ -14,6 +14,17 @@ inc_scores <- read_csv("paper-inputs/inc-scores.csv") %>%
   ungroup() %>%
   mutate(model = reorder(model, -model_rank, FUN=function(x) mean(x, na.rm=TRUE)))
 
+## number of unique opportunities for a prediction
+inc_scores %>%
+  group_by(target_end_date_1wk_ahead, unit, target) %>%
+  summarize(n()) %>%
+  nrow()
+
+## average rank
+inc_scores %>%
+  group_by(model) %>%
+  summarize(average_rank = mean(model_rank), total_n = n(), n_top_rank = sum(model_rank==1), pct_top = n_top_rank/total_n*100)
+
 
 # ggplot(inc_scores, aes(y=id, x=model, fill=model_rank)) +
 #   geom_tile() +
