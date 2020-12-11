5cb5047d780108a51ea8bb32d41037b44e3bca07
eycramer <ecramer@umass.edu>
2020-12-10 22:26:19 -0600

    update revision figure axis

--- code/figure-data_revisions.R
+++ code/figure-data_revisions.R
@@ -25,26 +25,26 @@ weekly_inc_deaths <- plyr::ldply(mondays, load_all_weeks)  #combine revisions in
 weekly_inc_deaths <- weekly_inc_deaths %>% 
   left_join(hub_locations %>% select(location = fips, location_name, abbreviation)) %>% #add location names
   mutate(MMWRweek = MMWRweek(weekly_inc_deaths$date)$MMWRweek) %>% #add epi week
-  filter(MMWRweek <= MMWRweek(last_timezero)$MMWRweek)  #filter to only include epi weeks in evaluation
+  filter(MMWRweek <= MMWRweek(last_timezero)$MMWRweek) #filter to only include epi weeks in evaluation
 
 #Create PDF of revisions in all locations 
 
 pdf('figures/data_revisions.pdf')
 
-for(i in 1:7) {
+for(i in 1:5) {
   fig_revisions <- ggplot(data = weekly_inc_deaths, aes(x = MMWRweek, y = inc, color = factor(revision_date))) +
     geom_line() +
     geom_point(size = 1) +
     theme_bw() +
-    scale_x_continuous(breaks = unique(weekly_inc_deaths$MMWRweek)[c(TRUE, FALSE)]) + 
+    scale_x_continuous(breaks = unique(weekly_inc_deaths$MMWRweek)[c(TRUE, FALSE)]) +
     #geom_vline(aes(xintercept = weekly_counts$first_fcast_date_impacted), linetype = "dashed") +
     #scale_x_date(date_labels = "%Y-%m-%d", breaks = c(mondays), name = "Date") +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 7),
-          legend.text = element_text(size = 7),
-          legend.title = element_text(size = 8)) +
-    ggforce::facet_wrap_paginate(~location_name, ncol = 2, nrow = 4,  scales = "free_y", page = i) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 6),
+          legend.text = element_text(size = 6),
+          legend.title = element_text(size = 7), legend.position="bottom", legend.box = "horizontal")+
+    ggforce::facet_wrap_paginate(~location_name, ncol = 3, nrow = 4,  scales = "free_y", page = i) +
     #facet_wrap(~abbreviation, scales = "free_y") +
-    ylab("Incident deaths reported") + labs(color = "Revision Date") + xlab("Epi Week")
+    ylab("Incident deaths reported") + labs(color = "Revision Date") + xlab("Epi Week") + ggtitle("Observed incident Deaths in each location over multiple weeks of data revisions")
   print(fig_revisions)
 }
 dev.off()
