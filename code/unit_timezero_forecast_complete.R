6c222281f06e7abfc008d3dc5ed5db2d5f6c5005
eycramer <ecramer@umass.edu>
2020-12-26 18:08:23 -0600

    update to match CHU

--- code/unit_timezero_forecast_complete.R
+++ code/unit_timezero_forecast_complete.R
@@ -1,7 +1,7 @@
 
 
 unit_timezero_forecast_complete <- function(dat, 
-    type = c("inc", "cum"), 
+    type = c("inc death", "cum death"), 
     horizons = 1:4,
     quantiles = c(0.01, 0.025, seq(0.05, 0.95, by=0.05), 0.975, 0.99)) {
     
@@ -11,7 +11,7 @@ unit_timezero_forecast_complete <- function(dat,
     
     tmp_target <- paste(horizons, "wk ahead", type, "death")
     
-    all_combos <- crossing(target = tmp_target, unit=unique(dat$unit), quantile=round(quantiles, digits=3))
+    all_combos <- crossing(target = tmp_target, location=unique(dat$location), quantile=round(quantiles, digits=3))
     missing_combos <- anti_join(all_combos, dat)
 
     if(nrow(missing_combos)==0){
