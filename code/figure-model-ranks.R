3476af3c0aa1c00ff2150836797b3e587b3ad78a
Nicholas Reich <nick@schoolph.umass.edu>
2021-01-01 22:14:08 -0500

    minor updates to model-ranks figure code

--- code/figure-model-ranks.R
+++ code/figure-model-ranks.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(covidHubUtils)
+library(ggridges)
 library(viridis)
 
 theme_set(theme_bw())
@@ -13,14 +14,21 @@ inc_scores <- read_csv("paper-inputs/inc-scores.csv") %>%
   ##filter(n_models >= 15) %>%
   arrange(wis) %>%
   mutate(model_rank = row_number(), rank_percentile = model_rank/n_models) %>%
+  arrange(-wis) %>%
+  mutate(rev_rank = (row_number()-1)/(n_models-1)) %>%
   ungroup() %>%
-  mutate(model = reorder(model, -model_rank, FUN=function(x) mean(x, na.rm=TRUE)))
+  mutate(model = reorder(model, rev_rank, FUN=function(x) quantile(x, probs=0.25, na.rm=TRUE)))
 
 ## number of unique opportunities for a prediction
 inc_scores %>%
   group_by(target_end_date_1wk_ahead, location_name, target) %>%
   summarize(n()) %>%
   nrow()
+
+## number of unique opportunities for a prediction by model
+inc_scores %>%
+  group_by(model) %>%
+  summarize(n()) 
 
 table(inc_scores$n_models)
 
@@ -50,58 +58,94 @@ p1 <- ggplot(inc_scores, aes(x=model, fill=factor(model_rank))) +
   xlab(NULL)
   
 
+# ggplot(inc_scores, aes(y=model, x=rank_percentile)) +
+#   geom_boxplot()
+
+inc_scores_sum <- inc_scores %>%
+  group_by(model) %>%
+  summarize(mean_rp = mean(rev_rank), 
+    q25_rp = quantile(rev_rank, probs=0.25),
+    median_rp = median(rev_rank), 
+    q75_rp = quantile(rev_rank, probs=0.75)) 
+
+# ggplot(inc_scores, aes(y=model, x=rank_percentile, height = ..density..)) +
+#   geom_density_ridges(scale = 1, stat = "density", trim = TRUE) + 
+#   scale_y_discrete(expand = c(0, 0)) +     # will generally have to set the `expand` option
+#   scale_x_continuous(expand = c(0, 0), limits=c(0,1)) +   # for both axes to remove unneeded padding
+#   coord_cartesian(clip = "off") + # to avoid clipping of the very top of the top ridgeline
+#   theme_ridges() +
+#   geom_vline(xintercept=1/17)
+# 
+# ggplot(inc_scores, aes(y=model, x=rank_percentile, height = ..density..)) +
+#   geom_density_ridges(stat = "binline", binwidth=.05, draw_baseline = F) +
+#   scale_y_discrete(expand = c(0, 0)) +     # will generally have to set the `expand` option
+#   scale_x_continuous(expand = c(0, 0), limits=c(0,1)) +   # for both axes to remove unneeded padding
+#   scale_fill_viridis_d(direction = -1, name="model rank") +
+#   coord_cartesian(clip = "off") + # to avoid clipping of the very top of the top ridgeline
+#   theme_ridges()
+# 
+# ggplot(inc_scores, aes(y=model, x=rank_percentile, fill = stat(x))) +
+#   geom_density_ridges_gradient(scale = 1) + 
+#   #geom_ridgeline_gradient() +
+#   scale_y_discrete(expand = c(0, 0)) +     # will generally have to set the `expand` option
+#   scale_x_continuous(expand = c(0, 0), limits=c(0,1)) +   # for both axes to remove unneeded padding
+#   scale_fill_viridis_c(direction = -1, name="model rank") +
+#   coord_cartesian(clip = "off") + # to avoid clipping of the very top of the top ridgeline
+#   theme_ridges()
+
+p2 <- ggplot(inc_scores, aes(y=model, x=rev_rank, fill = factor(stat(quantile)))) +
+  stat_density_ridges(
+    geom = "density_ridges_gradient", calc_ecdf = TRUE,
+    quantiles = 4, quantile_lines = TRUE
+  ) +
+  scale_fill_viridis_d(name = "Quartiles") +
+  scale_x_continuous(name="standardized rank", 
+    #expand=expansion(add=c(2, 1)/max(inc_scores$n_models)), 
+    limits=c(0,1))    # for both axes to remove unneeded padding
+  
+
 pdf(file = "figures/fig-model-ranks.pdf", width=8, height=5)
-print(p1)
+print(p2)
 dev.off()
 
 jpeg(file = "figures/fig-model-ranks.jpg", width=8, height=5, units="in", res=300)
-print(p1)
+print(p2)
 dev.off()
 
 
-ggplot(inc_scores, aes(y=model, x=rank_percentile)) +
-  geom_boxplot()
 
+## sensitivity analysis showing what we might expect if scores were uniform
+model_obs <- inc_scores %>%
+  group_by(model) %>%
+  summarize(nobs=n()) %>%
+  pull(nobs)
 
-library(ggridges)
-ggplot(inc_scores, aes(y=model, x=rank_percentile, height = ..density..)) +
-  geom_density_ridges(scale = 1, stat = "density", trim = TRUE) + 
-  scale_y_discrete(expand = c(0, 0)) +     # will generally have to set the `expand` option
-  scale_x_continuous(expand = c(0, 0), limits=c(0,1)) +   # for both axes to remove unneeded padding
-  coord_cartesian(clip = "off") + # to avoid clipping of the very top of the top ridgeline
-  theme_ridges() +
-  geom_vline(xintercept=1/17)
+forecasters <- rep(paste0("f_", letters[1:length(model_obs)]), 
+  times=model_obs)
 
-ggplot(inc_scores, aes(y=model, x=rank_percentile, height = ..density..)) +
-  geom_density_ridges(stat = "binline", binwidth=.05, draw_baseline = F) +
-  scale_y_discrete(expand = c(0, 0)) +     # will generally have to set the `expand` option
-  scale_x_continuous(expand = c(0, 0), limits=c(0,1)) +   # for both axes to remove unneeded padding
-  scale_fill_viridis_d(direction = -1, name="model rank") +
-  coord_cartesian(clip = "off") + # to avoid clipping of the very top of the top ridgeline
-  theme_ridges()
+## with same number of obs, different unit structure
+# fake_scores <- tibble(model = forecasters,
+#   score = runif(length(forecasters))) %>%
+#   group_by(model) %>%
+#   mutate(obs_num = row_number()) %>%
+#   group_by(obs_num) %>%
+#   mutate(rank = rank(-score)-1, nobs = n()-1, rp = rank/nobs)
 
-ggplot(inc_scores, aes(y=model, x=rank_percentile, fill = stat(x))) +
-  geom_density_ridges_gradient(scale = 1) + 
-  #geom_ridgeline_gradient() +
-  scale_y_discrete(expand = c(0, 0)) +     # will generally have to set the `expand` option
-  scale_x_continuous(expand = c(0, 0), limits=c(0,1)) +   # for both axes to remove unneeded padding
-  scale_fill_viridis_c(direction = -1, name="model rank") +
-  coord_cartesian(clip = "off") + # to avoid clipping of the very top of the top ridgeline
-  theme_ridges()
+## replacing original scores
+fake_scores <- inc_scores %>%
+  mutate(wis = runif(nrow(inc_scores))) %>%
+  group_by(model) %>%
+  mutate(obs_num = row_number()) %>%
+  group_by(obs_num) %>%
+  mutate(rank = rank(-wis)-1, nobs = n()-1, rp = rank/nobs)
 
-ggplot(inc_scores, aes(y=model, x=rank_percentile, fill = factor(stat(quantile)))) +
+ggplot(fake_scores, aes(y=model, x=rp, fill = factor(stat(quantile)))) +
   stat_density_ridges(
     geom = "density_ridges_gradient", calc_ecdf = TRUE,
-    quantiles = 5, quantile_lines = TRUE
+    quantiles = 4, quantile_lines = TRUE,
+    scale=1
   ) +
-  scale_fill_viridis_d(name = "Quartiles", direction=-1)
+  scale_fill_viridis_d(name = "Quartiles", direction=-1) +
+  scale_x_continuous(name="rank percentile", 
+    limits=c(0,1))    # for both axes to remove unneeded padding
 
-library(ggplot2)
-library(ggridges)
-library(viridis)
-ggplot(iris, aes(x=Sepal.Length, y=Species, fill = factor(stat(quantile)))) +
-  stat_density_ridges(
-    geom = "density_ridges_gradient", calc_ecdf = TRUE,
-    quantiles = 15, quantile_lines = TRUE
-  ) +
-  scale_fill_viridis_d(name = "Quantiles")
