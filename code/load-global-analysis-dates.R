55cc069e87310f03a6cc7f3c74c379dc4d42667f
Nicholas Reich <nick@schoolph.umass.edu>
2021-01-03 16:00:04 -0500

    updating scores and eligibility for draft paper.

--- code/load-global-analysis-dates.R
+++ code/load-global-analysis-dates.R
@@ -1,12 +1,12 @@
 require(covidHubUtils)
 
 ## retrieval date for truth data used in score calculation
-truth_date <- as.Date("2020-12-07")
+truth_date <- as.Date("2021-01-03")
 
 ## important dates for score calculations
 first_timezero <- as.Date("2020-05-12") ## earliest forecast_date in an eligible file 
-last_timezero4wk <- as.Date("2020-10-26") ## latest forecast_date in an eligible file, formerly was as.Date("2020-08-24")
-last_timezero <- as.Date("2020-11-16") ## latest forecast_date in an eligible file, based on 1 wk ahead 
+last_timezero4wk <- as.Date("2020-11-23") ## latest forecast_date in an eligible file, formerly was as.Date("2020-08-24"). Formerly as.Date("2020-10-26")
+last_timezero <- as.Date("2020-12-14") ## latest forecast_date in an eligible file, based on 1 wk ahead. Formerly as.Date("2020-11-16")
 
 ## range of target_week_end_dates
 first_1wk_target_end_date <- as.Date(calc_target_week_end_date(first_timezero, 1))
@@ -19,7 +19,7 @@ the_timezeros_inc <- seq(from = first_timezero, to = last_timezero, by="days")
 the_timezeros_eligibility <- seq(from = first_timezero, to = last_timezero4wk, by="days")
 
 ## maximum number of weeks missing that we allow before disqualifying a model
-MAXIMUM_MISSING_WEEKS <- 6 # formerly 3
+MAXIMUM_MISSING_WEEKS <- 9 # formerly 3
 UNITS_FOR_ELIGIBILITY <- covidHubUtils::hub_locations %>% 
   filter(geo_type == "state") %>% filter(location_name != "U.S. Minor Outlying Islands") %>% #added to remove counties
   mutate(for_scoring = abbreviation %in% c("US", datasets::state.abb)) %>%
@@ -33,10 +33,7 @@ NUM_WEEKS_INC <- num_weeks_forecasted - MAXIMUM_MISSING_WEEKS
 ## minumum number of models in a week to be considered eligible in a given week
 NUM_UNITS <- 25
 
-#Data issue Date
-version_date <- as.Date("2020-12-07")
-
 #Important dates
-last_date_evaluated <- as.Date(calc_target_week_end_date(version_date, -2)) #last date evaluated
+last_date_evaluated <- as.Date(calc_target_week_end_date(truth_date, -2)) #last date evaluated
 
 
