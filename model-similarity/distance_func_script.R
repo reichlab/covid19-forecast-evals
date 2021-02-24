4b933a5b5e717bf71396315961eee8c54d1b0a03
Nutcha Wattanachit <nutcha.wattanachit@gmail.com>
2021-02-23 19:14:47 -0500

    updated model similarity demo

--- model-similarity/distance_func_script.R
+++ model-similarity/distance_func_script.R
@@ -12,13 +12,22 @@ frame_format <- function(zoltr_frame){
 } 
 
 ## CvM function (we likely need to fix this)
-CvM_pairwise <- function(modelA,modelB,quantiles, spline_method,point_to_interpolate){
-  A <- spline(x=quantiles, y=modelA, method = spline_method,xout=point_to_interpolate)
-  B <- spline(x=quantiles, y=modelB, method = spline_method,xout=point_to_interpolate)
-  CvM_value <- cramer::cramer.test(A$y,B$y,just.statistic=TRUE)$statistic
-  return(CvM_value)
+distance_pairwise <- function(modelA,modelB,quantiles=NULL, spline_method,
+                              point_to_interpolate=NULL, distance){
+  if (distance!="approx_cramer"){
+    A <- spline(x=quantiles, y=modelA, method = spline_method,xout=point_to_interpolate)
+    B <- spline(x=quantiles, y=modelB, method = spline_method,xout=point_to_interpolate)
+    if(distance=="CvM"){
+      metric <- cramer::cramer.test(A$y,B$y,just.statistic=TRUE)$statistic
+      }
+    else if (distance=="cramer"){
+      metric <- energy::eqdist.e(c(A$y,B$y), rep(length(point_to_interpolate),2))/2
+      } 
+  } else if (distance=="approx_cramer"){
+    metric<- approx_cd(modelA, modelB)
+  }
+  return(metric)
 }
-
 # from http://estatcomp.github.io/henrique/exer_chap8.html
 cvm <- function(x, y){
   n <- length(x)
@@ -35,9 +44,36 @@ cvm <- function(x, y){
   return(cvm)
 }
 
+# cramer distance approximation for quantiles from Johannes
+approx_cd <- function(q_F, q_G){
+  # compute quantile levels from length of provided quantile vectors:
+  K <- length(q_F)
+  if(length(q_G) != K) stop("q_F and q_G need to be of the same length")
+  p <- (1:K)/(K + 1) # function assumes that the quantile levels are equally spaced
+  
+  # pool quantiles:
+  q0 <- c(q_F, q_G)
+  # vector of grouping variables, with 1 for values belonging to F, -1 for values 
+  # belonging to G
+  a0 <- c(rep(1, length(q_F)), rep(-1, length(q_G)))
+  
+  # re-order both vectors:
+  q <- q0[order(q0)]
+  a <- a0[order(q0)]
+  # and compute "how many quantiles ahead" F or G is at a given segment:
+  b <- abs(cumsum(a))
+  
+  # compute the lengths of segments defined by sorted quantiles:
+  diffs_q <- c(diff(q), 0) # zero necessary for indexing below, but we could put 
+  # anything (gets multiplied w zero)
+  # and approximate CD
+  cvm <- sum(diffs_q*b*(b + 1))/(K + 1)/(K)
+  return(mean(cvm))
+}
 ## a function that takes a data frame with single target-location and calculates CvM for pairwise combinations
 ## this returns a matrix of CvM for the models for a single target-location
-CvM_combination <- function(single_tarloc_frame,spline_method,point_to_interpolate){
+distance_combination <- function(single_tarloc_frame,spline_method=NULL,
+                                 point_to_interpolate=NULL,distance){
   # remove any models with NA for values for this target location (assuming all models have the same quantiles)
   single_tarloc_frame<- single_tarloc_frame[ , colSums(is.na(single_tarloc_frame)) == 0]
   quantiles <- single_tarloc_frame$quantile
@@ -50,7 +86,10 @@ CvM_combination <- function(single_tarloc_frame,spline_method,point_to_interpola
   nr <- nrow(eg)
   v <- vector(length=nr)
   for (i in 1:nr) {
-    cc <- CvM_pairwise(tmp[,eg[i,1]], tmp[,eg[i,2]],quantiles,spline_method,point_to_interpolate)
+    cc <- distance_pairwise(tmp[,eg[i,1]], tmp[,eg[i,2]],quantiles,
+                            spline_method,
+                            point_to_interpolate,
+                            distance)
     v[i] <- cc
   }
   single_tarloc_cvm <- matrix(v, nc, byrow=TRUE)
@@ -62,7 +101,8 @@ CvM_combination <- function(single_tarloc_frame,spline_method,point_to_interpola
 ## model_dataframe, this should be a union join with NA for non-overlapping target-locations
 ### single_tarloc_frame is a data frame containing one target-location created in the wrapper function 
 ### by filter on target and location
-build_CvM_frame <- function(model_dataframe, spline_method, target_list,point_to_interpolate){
+build_distance_frame <- function(model_dataframe, spline_method=NULL, 
+                                 target_list,point_to_interpolate=NULL, distance){
   library(tidyverse)
   library(tidyr)
   # remove point estimates and forecast_date
@@ -72,26 +112,25 @@ build_CvM_frame <- function(model_dataframe, spline_method, target_list,point_to
     main_frame <- main_frame %>%
       dplyr::select(-"forecast_date")
   }
-  ## apply CvM_combination function 
+  ## apply distance_combination function 
   locations <- unique(main_frame$location)
-  CvM_list <- list()
-  
+  dist_list <- list()
   for(loc in locations){
       for(target in target_list){
         name <- paste(loc,target,sep="_") 
-        single_matrix <- main_frame %>%
-          dplyr::filter(location==loc,
-                        target_variable==target) %>%
-          dplyr::arrange(target_variable,location,quantile) %>%
-          CvM_combination(.,spline_method,point_to_interpolate)
-        CvM_list <- c(CvM_list,list(name = single_matrix))
+          single_matrix <- main_frame %>%
+            dplyr::filter(location==loc,
+                          target_variable==target) %>%
+            dplyr::arrange(target_variable,location,quantile) %>%
+            distance_combination(.,spline_method,point_to_interpolate, distance)
+        dist_list <- c(dist_list,list(name = single_matrix))
       }
     }
-  return(CvM_list)
+  return(dist_list)
 }
 
 # a function that takes a matrix and plot a heatmap
-CvM_heatmap <- function(single_tarloc_cvm){
+distance_heatmap <- function(single_tarloc_cvm){
   dat <- data.frame(var1=rownames(single_tarloc_cvm)[row(single_tarloc_cvm)], 
                     var2=colnames(single_tarloc_cvm)[col(single_tarloc_cvm)], 
                     cvm=c(single_tarloc_cvm))
@@ -106,25 +145,33 @@ CvM_heatmap <- function(single_tarloc_cvm){
 
 
 ## comparison
-CvM_comparision_pairwise <- function(modelA,modelB,quantiles, spline_method,point_to_interpolate,est_method){
+distance_comparision_pairwise <- function(modelA,modelB,quantiles=NULL, spline_method=NULL,
+                                          point_to_interpolate=NULL,est_method){
+  if(est_method!=6){
   A <- spline(x=quantiles, y=modelA, method = spline_method,xout=point_to_interpolate)
   B <- spline(x=quantiles, y=modelB, method = spline_method,xout=point_to_interpolate)
   if(est_method==1){
-    CvM_value <- cramer::cramer.test(A$y,B$y,just.statistic=TRUE)$statistic
+    value <- cramer::cramer.test(A$y,B$y,just.statistic=TRUE)$statistic
   } 
   else if(est_method==2){
-    CvM_value <- twosamples::cvm_stat(A$y,B$y,power=2)
+    value <- twosamples::cvm_stat(A$y,B$y,power=2)
   }
   else if(est_method==3){
-    CvM_value <- CDFt::CramerVonMisesTwoSamples(A$y,B$y)
+    value <- CDFt::CramerVonMisesTwoSamples(A$y,B$y)
   }
   else if(est_method==4){
-    CvM_value <- CvM_value <- cvm(A$y,B$y)
+    value <- cvm(A$y,B$y)
   }
-  return(CvM_value)
+  else if(est_method==5){
+    value <- energy::eqdist.e(as.matrix(c(A$y,B$y)), rep(length(point_to_interpolate),2))/2
+  }} else {
+    value <- approx_cd(modelA, modelB)
+  }
+  return(value)
 }
 
-CvM_combination_compare <- function(single_tarloc_frame,spline_method,point_to_interpolate,est_method){
+distance_combination_compare <- function(single_tarloc_frame,spline_method=NULL,
+                                    point_to_interpolate=NULL,est_method){
   # remove any models with NA for values for this target location (assuming all models have the same quantiles)
   single_tarloc_frame<- single_tarloc_frame[ , colSums(is.na(single_tarloc_frame)) == 0]
   quantiles <- single_tarloc_frame$quantile
@@ -137,7 +184,7 @@ CvM_combination_compare <- function(single_tarloc_frame,spline_method,point_to_i
   nr <- nrow(eg)
   v <- vector(length=nr)
   for (i in 1:nr) {
-    cc <- CvM_comparision_pairwise(tmp[,eg[i,1]], tmp[,eg[i,2]],quantiles,
+    cc <- distance_comparision_pairwise(tmp[,eg[i,1]], tmp[,eg[i,2]],quantiles,
                                    spline_method,point_to_interpolate,est_method)
     v[i] <- cc
   }
@@ -145,7 +192,8 @@ CvM_combination_compare <- function(single_tarloc_frame,spline_method,point_to_i
   dimnames(single_tarloc_cvm) <- list(cnames, cnames)
   return(single_tarloc_cvm)
 }
-build_CvM_frame_compare <- function(model_dataframe, spline_method, target_list,point_to_interpolate,est_method){
+build_distance_frame_compare <- function(model_dataframe, spline_method=NULL, target_list,
+                                    point_to_interpolate=NULL,est_method){
   library(tidyverse)
   library(tidyr)
   # remove point estimates and forecast_date
@@ -155,7 +203,7 @@ build_CvM_frame_compare <- function(model_dataframe, spline_method, target_list,
     main_frame <- main_frame %>%
       dplyr::select(-"forecast_date")
   }
-  ## apply CvM_combination function 
+  ## apply distance_combination function 
   locations <- unique(main_frame$location)
   CvM_list <- list()
   
@@ -166,7 +214,7 @@ build_CvM_frame_compare <- function(model_dataframe, spline_method, target_list,
         dplyr::filter(location==loc,
                       target_variable==target) %>%
         dplyr::arrange(target_variable,location,quantile) %>%
-        CvM_combination_compare(.,spline_method,point_to_interpolate,est_method)
+        distance_combination_compare(.,spline_method,point_to_interpolate,est_method)
       CvM_list <- c(CvM_list,list(name = single_matrix))
     }
   }
@@ -180,7 +228,7 @@ sample_quantile <- function(x, prob) {
   approx(seq(0, 1, length = n), x, prob)$y
 }
 
-toy_compare <- function(single_frame,spline_method,point_to_interpolate,est_method){
+toy_compare <- function(single_frame,spline_method=NULL,point_to_interpolate=NULL,est_method){
   # remove any models with NA for values for this target location (assuming all models have the same quantiles)
   quantiles <- single_frame$q
   # pairwise column calculation
@@ -192,7 +240,7 @@ toy_compare <- function(single_frame,spline_method,point_to_interpolate,est_meth
   nr <- nrow(eg)
   v <- vector(length=nr)
   for (i in 1:nr) {
-    cc <- CvM_comparision_pairwise(tmp[,eg[i,1]], tmp[,eg[i,2]],quantiles,
+    cc <- distance_comparision_pairwise(tmp[,eg[i,1]], tmp[,eg[i,2]],quantiles,
                                    spline_method,point_to_interpolate,est_method)
     v[i] <- cc
   }
