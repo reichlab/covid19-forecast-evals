fba3f43915a55932e7ed164c3bff7baa0017068d
Nicholas Reich <nick@schoolph.umass.edu>
2021-05-04 09:09:04 -0400

    random updates to scripts that had not been committed from a while back.

--- code/check-forecast-versions.R
+++ code/check-forecast-versions.R
@@ -11,13 +11,14 @@ forecast_versions <- vers %>% mutate(
   v2_github_timestamp = as.POSIXct(v2_github_timestamp, format="%m/%d/%y-%X"),
   v3_github_timestamp = as.POSIXct(v3_github_timestamp, format="%m/%d/%y-%X"),
   v2_diff_hrs = difftime(v2_github_timestamp, v1_github_timestamp, units="hours"), 
-  v3_diff_hrs = difftime(v3_github_timestamp, v2_github_timestamp, units="hours"))
+  v3_diff_hrs = difftime(v3_github_timestamp, v2_github_timestamp, units="hours"),
+  last_submission_timestamp = pmax(v1_github_timestamp, v2_github_timestamp, v3_github_timestamp, na.rm=TRUE))
 
 model_eligibility <- read_csv("paper-inputs/model-eligibility-inc.csv") %>%
   left_join(forecast_versions, by=c("model" = "model", "forecast_date" = "timezero")) %>%
   filter(model %in% models) %>%
   mutate(deadline = as.POSIXct(paste(target_end_date_1wk_ahead-5, "18:00:00")),
-    deadline_diff = difftime(v1_github_timestamp, deadline, units="hours"))
+    deadline_diff = difftime(last_submission_timestamp, deadline, units="hours"))
 
 ## manual check that every v1 in zoltar has no additional versions
 v1_idx <- model_eligibility$in_zoltar=="v1"
@@ -35,11 +36,26 @@ sum(model_eligibility$v2_diff_hrs>24 | (model_eligibility$v2_diff_hrs+model_elig
 
 nrow(model_eligibility)
 
-## count number of individual submissions more than 24 hours after the deadline
+## count number of individual submissions whose evaluated forecast was 
+## submitted more than 24 hours after the deadline
 model_eligibility %>%
-  filter(model != "COVIDhub-ensemble", deadline_diff>24) %>% #print(n=Inf)
+  filter(deadline_diff>24) %>% #print(n=Inf)
   nrow()
-  
+
+model_eligibility %>%
+  filter(!(model %in% c("COVIDhub-baseline", "COVIDhub-ensemble")), deadline_diff>24) %>% #print(n=Inf)
+  group_by(model) %>% 
+  summarize(nobs=n()) %>% arrange(-nobs)
+
+## check that all files in model_eligibility are in inc_scores
+inc_scores <- read_csv("paper-inputs/inc-scores.csv") %>%
+  group_by(model, forecast_date)%>%
+  summarize(nobs=n()) %>%
+  full_join(model_eligibility) %>%
+  group_by(model, forecast_date) %>%
+  summarize(n())
+
+## GT-DeepCovid has 1 repeated row in the forecast versions file, with forecast_date = 2020-11-09
 
 write_csv(forecast_versions, "paper-inputs/forecast-versions.csv")
 
