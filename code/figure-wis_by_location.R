6e062ff8a882d037f8480b2856366a8d4be76e51
eycramer <ecramer@umass.edu>
2021-05-18 08:46:07 -0500

    add phases, update order

--- code/figure-wis_by_location.R
+++ code/figure-wis_by_location.R
@@ -16,14 +16,17 @@ model_levels <- read_csv("paper-inputs/table-overall-performance.csv") %>%
 end_date <- truth_date
 
 
-theme_set(theme_bw())
-data("hub_locations")
+# theme_set(theme_bw())
+# data("hub_locations")
 
 inc_scores <- read_csv("paper-inputs/inc-scores.csv") %>%
   filter(location_name %in% (hub_locations %>% filter(geo_type == "state") %>% pull(location_name))) %>%
   filter(location_name != "American Samoa" & location_name != "Northern Mariana Islands") %>%
   filter(horizon %in% c(1:4)) %>%
-  filter(forecast_date <= last_timezero4wk) 
+  filter(forecast_date <= last_timezero4wk)  %>%
+  mutate(seasonal_phase = case_when(forecast_date < first_forecast_date_summer ~ "spring",
+                                    forecast_date >= first_forecast_date_summer & forecast_date  < first_forecast_date_winter ~ "summer",
+                                    forecast_date >= first_forecast_date_winter ~ "winter"))
 
 
 #Count number of weeks a model has submitted 
@@ -146,7 +149,6 @@ for(i in seq_along(locations)){
 }
 
 
-
 ## plot of true data by state, tiled
 truth_dat <- load_truth(truth_source = "JHU",
                         target_variable = "inc death") %>%
@@ -169,6 +171,7 @@ average_by_loc_to_plot <- average_by_loc %>%
          model = fct_relevel(model, model_levels)) %>%
   filter(!is.na(relative_wis)) 
 
+average_by_loc_to_plot$model <- reorder(average_by_loc_to_plot$model, average_by_loc_to_plot$relative_wis)
 
 # plot:
 fig_wis_loc <- ggplot(average_by_loc_to_plot, 
@@ -199,4 +202,261 @@ jpeg(file = "figures/fig-wis-location.jpg", width=8, height=8, units="in", res=3
 print(fig_wis_loc)
 dev.off()
 
+
+###phases chart
+library(scoringutils)
+
+spring_phase <- inc_scores %>% filter(seasonal_phase == "spring")
+
+scores <- spring_phase %>% select("model", "forecast_date", "location", "location_name", "horizon", "abs_error", "wis") %>% droplevels()
+
+# the included models and locations:
+models <- unique(scores$model)
+locations <- unique(scores$location)
+location_names <- unique(scores$location_name)
+
+
+# compute pairwise and relative WIS for each location separately:
+for(i in seq_along(locations)){
+  
+  # select location:
+  loc <- locations[i]
+  loc_name <- location_names[i]
+  
+  # matrix to store:
+  results_ratio_temp <- matrix(ncol = length(models),
+                               nrow = length(models),
+                               dimnames = list(models, models)) 
+  
+  # run pairwise comparison for chosen location:
+  for(mx in seq_along(models)){
+    for(my in 1:mx){
+      pwc <- pairwise_comparison(scores = scores, mx = models[mx], my = models[my],
+                                 permutation_test = FALSE, # disable permutation test to speed up things
+                                 subset = scores$location == loc) # this will subset to the respective location inside the function
+      results_ratio_temp[mx, my] <- pwc$ratio
+      results_ratio_temp[my, mx] <- 1/pwc$ratio
+    }
+  }
+  
+  # compute the geometric means etc
+  ind_baseline <- which(rownames(results_ratio_temp) == "COVIDhub-baseline")
+  geom_mean_ratios_temp <- exp(rowMeans(log(results_ratio_temp[, -ind_baseline]), na.rm = TRUE))
+  ratios_baseline_temp <- results_ratio_temp[, "COVIDhub-baseline"]
+  ratios_baseline2_temp <- geom_mean_ratios_temp/geom_mean_ratios_temp["COVIDhub-baseline"]
+  
+  # summarize results:
+  to_add <- data.frame(model = names(ratios_baseline2_temp),
+                       location = loc,
+                       location_name = loc_name,
+                       relative_wis = ratios_baseline2_temp,
+                       log_relative_wis = log(ratios_baseline2_temp))
+  
+  # append to already stored:
+  if(i == 1){ # initialize at first location
+    average_by_loc <- to_add
+  }else{
+    average_by_loc <- rbind(average_by_loc, to_add)
+  }
+  
+  cat("Finished", loc_name, "\n")
+}
+
+average_by_loc_spring <- average_by_loc %>%
+  mutate(seasonal_phase = "spring")
+
+
+#Summer
+summer_phase <- inc_scores %>% filter(seasonal_phase == "summer")
+
+scores <- summer_phase %>% select("model", "forecast_date", "location", "location_name", "horizon", "abs_error", "wis") %>% droplevels()
+
+# the included models and locations:
+models <- unique(scores$model)
+locations <- unique(scores$location)
+location_names <- unique(scores$location_name)
+
+
+# compute pairwise and relative WIS for each location separately:
+for(i in seq_along(locations)){
+  
+  # select location:
+  loc <- locations[i]
+  loc_name <- location_names[i]
+  
+  # matrix to store:
+  results_ratio_temp <- matrix(ncol = length(models),
+                               nrow = length(models),
+                               dimnames = list(models, models)) 
+  
+  # run pairwise comparison for chosen location:
+  for(mx in seq_along(models)){
+    for(my in 1:mx){
+      pwc <- pairwise_comparison(scores = scores, mx = models[mx], my = models[my],
+                                 permutation_test = FALSE, # disable permutation test to speed up things
+                                 subset = scores$location == loc) # this will subset to the respective location inside the function
+      results_ratio_temp[mx, my] <- pwc$ratio
+      results_ratio_temp[my, mx] <- 1/pwc$ratio
+    }
+  }
+  
+  # compute the geometric means etc
+  ind_baseline <- which(rownames(results_ratio_temp) == "COVIDhub-baseline")
+  geom_mean_ratios_temp <- exp(rowMeans(log(results_ratio_temp[, -ind_baseline]), na.rm = TRUE))
+  ratios_baseline_temp <- results_ratio_temp[, "COVIDhub-baseline"]
+  ratios_baseline2_temp <- geom_mean_ratios_temp/geom_mean_ratios_temp["COVIDhub-baseline"]
+  
+  # summarize results:
+  to_add <- data.frame(model = names(ratios_baseline2_temp),
+                       location = loc,
+                       location_name = loc_name,
+                       relative_wis = ratios_baseline2_temp,
+                       log_relative_wis = log(ratios_baseline2_temp))
+  
+  # append to already stored:
+  if(i == 1){ # initialize at first location
+    average_by_loc <- to_add
+  }else{
+    average_by_loc <- rbind(average_by_loc, to_add)
+  }
+  
+  cat("Finished", loc_name, "\n")
+}
+
+average_by_loc_summer <- average_by_loc %>%
+  mutate(seasonal_phase = "summer")
+
+
+#Winter
+winter_phase <- inc_scores %>% filter(seasonal_phase == "winter")
+
+scores <- winter_phase %>% select("model", "forecast_date", "location", "location_name", "horizon", "abs_error", "wis") %>% droplevels()
+
+# the included models and locations:
+models <- unique(scores$model)
+locations <- unique(scores$location)
+location_names <- unique(scores$location_name)
+
+
+# compute pairwise and relative WIS for each location separately:
+for(i in seq_along(locations)){
+  
+  # select location:
+  loc <- locations[i]
+  loc_name <- location_names[i]
+  
+  # matrix to store:
+  results_ratio_temp <- matrix(ncol = length(models),
+                               nrow = length(models),
+                               dimnames = list(models, models)) 
+  
+  # run pairwise comparison for chosen location:
+  for(mx in seq_along(models)){
+    for(my in 1:mx){
+      pwc <- pairwise_comparison(scores = scores, mx = models[mx], my = models[my],
+                                 permutation_test = FALSE, # disable permutation test to speed up things
+                                 subset = scores$location == loc) # this will subset to the respective location inside the function
+      results_ratio_temp[mx, my] <- pwc$ratio
+      results_ratio_temp[my, mx] <- 1/pwc$ratio
+    }
+  }
+  
+  # compute the geometric means etc
+  ind_baseline <- which(rownames(results_ratio_temp) == "COVIDhub-baseline")
+  geom_mean_ratios_temp <- exp(rowMeans(log(results_ratio_temp[, -ind_baseline]), na.rm = TRUE))
+  ratios_baseline_temp <- results_ratio_temp[, "COVIDhub-baseline"]
+  ratios_baseline2_temp <- geom_mean_ratios_temp/geom_mean_ratios_temp["COVIDhub-baseline"]
+  
+  # summarize results:
+  to_add <- data.frame(model = names(ratios_baseline2_temp),
+                       location = loc,
+                       location_name = loc_name,
+                       relative_wis = ratios_baseline2_temp,
+                       log_relative_wis = log(ratios_baseline2_temp))
+  
+  # append to already stored:
+  if(i == 1){ # initialize at first location
+    average_by_loc <- to_add
+  }else{
+    average_by_loc <- rbind(average_by_loc, to_add)
+  }
+  
+  cat("Finished", loc_name, "\n")
+}
+
+average_by_loc_winter <- average_by_loc %>%
+  mutate(seasonal_phase = "winter")
+
+
+average_by_loc_to_plot <- average_by_loc %>%
+  filter(location_name != "American Samoa" & location_name != "Northern Mariana Islands") %>%
+  mutate(location_name = fct_relevel(location_name, levels(truth_dat)),
+         relative_wis_text = sprintf("%.1f", round(relative_wis, 1)),
+         log_relative_wis = log2(relative_wis),
+         model = fct_relevel(model, model_levels)) %>%
+  filter(!is.na(relative_wis)) 
+
+average_by_loc_to_plot$model <- reorder(average_by_loc_to_plot$model, average_by_loc_to_plot$relative_wis)
+
+
+
+# 
+# pairwise_df_spring <- scoringutils::pairwise_comparison(
+#   scores = spring_phase, 
+#   metric = "interval_score",
+#   baseline = "COVIDhub-baseline",
+#   summarise_by = c("model", "location_name"))  %>%
+#   mutate(seasonal_phase = "spring")
+# 
+# pairwise_df_summer <- scoringutils::pairwise_comparison(
+#   scores = inc_scores %>% filter(seasonal_phase == "summer") %>% mutate(interval_score = wis), 
+#   metric = "interval_score",
+#   baseline = "COVIDhub-baseline",
+#   summarise_by = c("model", "location_name"))  %>%
+#   mutate(seasonal_phase = "summer")
+# 
+# 
+# pairwise_df_winter <- scoringutils::pairwise_comparison(
+#   scores = inc_scores %>% filter(seasonal_phase == "winter") %>% mutate(interval_score = wis), 
+#   metric = "interval_score",
+#   baseline = "COVIDhub-baseline",
+#   summarise_by = c("model", "location_name")) %>%
+#   mutate(seasonal_phase = "winter")
+
+order_models <- average_by_loc %>% 
+  group_by(model) %>% summarise(wis = mean(relative_wis, na.rm = T))  %>%
+  arrange((wis)) %>% pull(model)
+
+to_plot_phase <- rbind(average_by_loc_spring, average_by_loc_summer,average_by_loc_winter) 
+
+to_plot_phase <- to_plot_phase %>%
+  mutate(model = fct_relevel(model, order_models))
+
+fig_wis_loc <- ggplot(to_plot_phase,  aes(x=model, y=location_name, 
+                          fill= scales::oob_squish(log_relative_wis, range = c(- 2.584963, 2.584963)))) +
+  geom_tile() +
+  facet_wrap(~seasonal_phase) + 
+  geom_text(aes(label = round(relative_wis,1)), size = 2) + # I adapted the rounding
+  scale_fill_gradient2(low = "steelblue", high = "red", midpoint = 0, na.value = "grey50", 
+                       name = "Relative WIS", 
+                       breaks = c(-2,-1,0,1,2), 
+                       labels =c("0.25", 0.5, 1, 2, 4)) + 
+  xlab(NULL) + ylab(NULL) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 9), 
+                                   # color=ifelse(
+                                   #   levels(to_plot_phase$model) %in% models_to_highlight,
+                                   #   "red", "black")),
+        axis.title.x = element_text(size = 9),
+        axis.text.y = element_text(size = 9),
+        title = element_text(size = 9)) +
+  scale_x_discrete(labels=c("IHME-CurveFit" = "IHME-SEIR"))
+
+
+pdf(file = "figures/fig-wis-location_phase.pdf", width=16, height=8)
+print(fig_wis_loc)
+dev.off()
+
+jpeg(file = "figures/fig-wis-location_phase.jpg", width=16, height=8, units="in", res=300)
+print(fig_wis_loc)
+dev.off()
 
