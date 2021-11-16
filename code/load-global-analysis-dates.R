8e2c41f210302067d42e7de1ec450f3121d195cd
eycramer <ecramer@umass.edu>
2021-11-16 11:39:39 -0500

    update for 79 weeks

--- code/load-global-analysis-dates.R
+++ code/load-global-analysis-dates.R
@@ -2,15 +2,19 @@ require(covidHubUtils)
 require(tidyverse)
 
 ## retrieval date for truth data used in score calculation
-truth_date <- as.Date("2021-10-18") # previously as.Date("2021-05-25") #previously 2021-05-03
+truth_date <- as.Date("2021-11-16")
+  # previously as.Date("2021-10-18") 
+  # previously as.Date("2021-05-25") 
+  #previously 2021-05-03
 
 ## spreadsheet for keeping track of dates
+## https://docs.google.com/spreadsheets/d/1u6PTlQoNgIyWwE_msswftg2P3U0JOmt5pwIRciAq0Wk/edit#gid=1491635626
 ## https://docs.google.com/spreadsheets/d/1u6PTlQoNgIyWwE_msswftg2P3U0JOmt5pwIRciAq0Wk/edit#gid=1071484175
 
 ## important dates for score calculations
 first_timezero <- as.Date("2020-04-21") ## earliest forecast_date in an eligible file 
-last_timezero <- as.Date("2021-09-27") #previously as.Date("2021-04-26") ## latest forecast_date for 1 wk ahead in an eligible file, based on 1 wk ahead. 
-last_timezero4wk <- as.Date("2021-09-06") #previously as.Date("2021-04-05") ## latest forecast_date for 4 wk ahead in an eligible file
+last_timezero <- as.Date("2021-10-25") #as.Date("2021-09-27") #previously as.Date("2021-04-26") ## latest forecast_date for 1 wk ahead in an eligible file, based on 1 wk ahead. 
+last_timezero4wk <- as.Date("2021-10-04") #as.Date("2021-09-06") #previously as.Date("2021-04-05") ## latest forecast_date for 4 wk ahead in an eligible file
  
 ## range of target_week_end_dates. 
 first_target_end_date <- as.Date(calc_target_week_end_date(first_timezero, 1))
@@ -21,11 +25,12 @@ last_target_end_date <- as.Date(calc_target_week_end_date(last_timezero, 1)) #la
 ## these include start/end dates for each inc target
 the_timezeros <- seq(from = first_timezero, to = last_timezero, by="days") #previously 53 eval weeks worth of timezeros 
 the_timezeros_4wk <- seq(from = first_timezero, to = last_timezero4wk, by="days") 
-the_target_end_dates <- seq(from = first_target_end_date, last_target_end_date, by = "week") #75 target_end_dates 
+the_target_end_dates <- seq(from = first_target_end_date, last_target_end_date, by = "week") #79 target_end_dates 
 
   
 ## maximum number of weeks missing that we allow before disqualifying a model
-MAXIMUM_MISSING_WEEKS <- 69 # 75 total target_end_dates, need to have submitted at a minimum 6 weeks for inclusion in spring
+MAXIMUM_MISSING_WEEKS <- 73 #79 total target_end_dates, need to have submitted at a minimum 6 weeks for inclusion in spring
+                            #75 total target_end_dates, need to have submitted at a minimum 6 weeks for inclusion in spring
                             #formerly 45. Updated b/c there are now only 10 weeks in spring period and need to include models that have submitted for 6 weeks in spring
                             #formerly 17 # formerly 3 
 
@@ -40,9 +45,9 @@ UNITS_FOR_ELIGIBILITY <-covidHubUtils::hub_locations %>%
 ## max number of weeks missing for overall analysis (not stratified by phase)
 num_weeks_forecasted <- length(the_target_end_dates)
 
-MAXIMUM_MISSING_WEEKS_OVERALL <- round(num_weeks_forecasted*0.4, 0) #Team can be missing up to 40% #formerly 21. #changed b/c there are 53 weeks and 53*0.6 = 31.8 #formerly 17 #formerly 3 
+MAXIMUM_MISSING_WEEKS_OVERALL <- round(num_weeks_forecasted*0.4, 0) #missing up to 32 weeks #Team can be missing up to 40% #formerly 21. #changed b/c there are 53 weeks and 53*0.6 = 31.8 #formerly 17 #formerly 3 
 
-NUM_WEEKS_INC <- num_weeks_forecasted - MAXIMUM_MISSING_WEEKS_OVERALL  #for inclusion overall, must have submitted at least 60% of weeks
+NUM_WEEKS_INC <- num_weeks_forecasted - MAXIMUM_MISSING_WEEKS_OVERALL  #47 #for inclusion overall, must have submitted at least 60% of weeks
 
 
 ## minumum number of locations in a week to be considered eligible in a given week
